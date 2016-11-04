bfdb684eb91b9ddf66bf6b17fefc0cad13cd87b9
hewgouw <hewgouw@gmail.com>
2016-11-04 20:22:12 +0100

    Now allows POST requests

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-function get(req, res) {
+function request(req, res) {
 	
 	var urlParts = req.originalUrl.replace("/api/", "").split("/");
     var sectionName = urlParts[0];
@@ -10,7 +10,7 @@ function get(req, res) {
 		var action = section[actionName];
         if(action) {
         
-			return action(req, res, urlParts[2]);
+			return action(req, res);
         }
     }
     
@@ -26,23 +26,30 @@ var api = {
 
 api.statusCodes.error400 = function (req, res) {
     res.status(400)
-        .send('Bad request');
+       .send('Bad request');
 };
 api.statusCodes.error404 = function (req, res) {
     res.status(404)
-        .send('Not found');
+       .send('Not found');
 };
 
-/* Projects */
+/* myModule */
 
 api.myModule.getTodaysDate = function (req, res) {
     var date = new Date();
 
-    res.send("Todays date: ", date);
+    res.status(200)
+       .send("Todays date: " + date);
 };
 
-
+api.myModule.postMeSomething = function (req, res) {
+    var data = req.body;
+    
+    res.status(200)
+       .send("You sent me: " +              
+             JSON.stringify(data));
+}
 
 module.exports = {
-    get: get
+    request: request
 };
--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+
 var api = require('./api.js');
+
 app.get('/api*', function(req, res) {
-    api.get(req, res);
+    api.request(req, res);
 });
+
+app.post('/api*', function(req, res) {
+    api.request(req, res);
+});
+
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
